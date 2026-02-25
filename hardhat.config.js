5daabd5558b5bf3d85f18f49ccbb383014239e08
RedBox-XK <85796047+redbox-xk@users.noreply.github.com>
2026-02-25 15:28:55 +0100

    Configure Hardhat with Solidity version and networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,1 +1,12 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    mainnet: {
+      url: process.env.MAINNET_RPC,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
+};
